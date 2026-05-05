ea6be9acf8855f88e2f745cdaaf8790d0c0666c4
fayazullahshafiqi-maker <fayazullahshafiqi@gmail.com>
2026-05-05 13:56:19 +1000

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,13 @@
+def scrape_marketplace(query):
+    # TEMP SIMULATED DATA (stable version first)
+
+    return [
+        {
+            "title": f"🔥 Deal Found: Toyota {query}",
+            "url": "https://facebook.com/marketplace/example1"
+        },
+        {
+            "title": f"🚗 Cheap Listing Alert: {query}",
+            "url": "https://facebook.com/marketplace/example2"
+        }
+    ]
